debbf058bed471c385354fa7427469fd69fd20f2
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2020-12-25 13:43:33 +0330

    Add SocialMedia.cs model

---
+++ AMZEnterprisePortfolio/Models/SocialMedia.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AMZEnterprisePortfolio.Data;
+
+namespace AMZEnterprisePortfolio.Models
+{
+    /// <summary>
+    /// Social entity
+    /// </summary>
+    public class SocialMedia : IEntity
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string Title { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string Url { get; set; }
+        [Display(Name = "Icon Css")]
+        [Required]
+        [MaxLength(256)]
+        public string IconCss { get; set; }
+    }
+}
