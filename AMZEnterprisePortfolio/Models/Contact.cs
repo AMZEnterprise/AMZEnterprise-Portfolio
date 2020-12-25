45dc8bc4d0f3ef55d9ee6574a5b8ba5c505a8d23
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2020-12-25 13:44:30 +0330

    Add Contact.cs model

---
+++ AMZEnterprisePortfolio/Models/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using AMZEnterprisePortfolio.Data;
+
+namespace AMZEnterprisePortfolio.Models
+{
+    /// <summary>
+    /// Contact entity
+    /// </summary>
+    public class Contact : IEntity
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string FullName { get; set; }
+        [Required]
+        [MaxLength(256)]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string Subject { get; set; }
+        [Required]
+        [MaxLength(1000)]
+        public string Message { get; set; }
+        [MaxLength(256)]
+        public string Ip { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
