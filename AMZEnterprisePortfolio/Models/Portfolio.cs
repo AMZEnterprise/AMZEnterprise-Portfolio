ac86b330ae079e0716023e83bf19cb0a05991fde
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2020-12-25 13:45:45 +0330

    Add Portfolio.cs model

---
+++ AMZEnterprisePortfolio/Models/Portfolio.cs
@@ -0,0 +1,38 @@
+﻿using AMZEnterprisePortfolio.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AMZEnterprisePortfolio.Models
+{
+    /// <summary>
+    /// Portfolio entity
+    /// </summary>
+    public class Portfolio : IEntity
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string Title { get; set; }
+        [Display(Name = "Short Description")]
+        [Required]
+        [MaxLength(1000)]
+        public string ShortDescription { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Display(Name = "Employer FullName")]
+        [Required]
+        [MaxLength(256)]
+        public string EmployerFullName { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string Technologies { get; set; }
+        public Guid FilePathGuid { get; set; }
+        [NotMapped]
+        [Display(Name = "Portfolio Image")]
+        public string FilePath { get; set; }
+        [Display(Name = "Portfolio Type")]
+        public PortfolioType PortfolioType { get; set; }
+    }
+}
---
+++ AMZEnterprisePortfolio/Models/PortfolioType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMZEnterprisePortfolio.Models
+{
+    /// <summary>
+    /// Portfolio Type
+    /// </summary>
+    public enum PortfolioType
+    {
+        Website = 1000,
+        Application = 2000,
+        Seo = 3000,
+        Other = 4000
+    }
+}
