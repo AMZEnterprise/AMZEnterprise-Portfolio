5c74d0bbd3abc0c68b9081ffdf881544dc3c811a
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2020-12-25 13:44:44 +0330

    Add Favor.cs model

---
+++ AMZEnterprisePortfolio/Models/Favor.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AMZEnterprisePortfolio.Data;
+
+namespace AMZEnterprisePortfolio.Models
+{
+    /// <summary>
+    /// Favor entity
+    /// </summary>
+    public class Favor : IEntity
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string Title { get; set; }
+        [Required]
+        [MaxLength(1000)]
+        public string Description { get; set; }
+    }
+}
