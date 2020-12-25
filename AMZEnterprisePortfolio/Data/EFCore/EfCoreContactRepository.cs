4ca5e4f106c7658f929a8d01c7798cd1e5cd0fe8
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2020-12-25 13:53:04 +0330

    Add EfCoreContactRepository.cs

---
+++ AMZEnterprisePortfolio/Data/EFCore/EfCoreContactRepository.cs
@@ -0,0 +1,14 @@
+﻿using AMZEnterprisePortfolio.Models;
+
+namespace AMZEnterprisePortfolio.Data.EFCore
+{
+    /// <summary>
+    /// Contact repository ef core implementation
+    /// </summary>
+    public class EfCoreContactRepository : EfCoreRepository<Contact,ApplicationDbContext>
+    {
+        public EfCoreContactRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
