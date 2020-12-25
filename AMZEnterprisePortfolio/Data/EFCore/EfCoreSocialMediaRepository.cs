6807471979179e652a11999ce038a04acb3cd544
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2020-12-25 13:54:08 +0330

    Add EfCoreSocialMediaRepository.cs

---
+++ AMZEnterprisePortfolio/Data/EFCore/EfCoreSocialMediaRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AMZEnterprisePortfolio.Models;
+
+namespace AMZEnterprisePortfolio.Data.EFCore
+{
+    /// <summary>
+    /// Social media repository ef core implementation
+    /// </summary>
+    public class EfCoreSocialMediaRepository : EfCoreRepository<SocialMedia, ApplicationDbContext>
+    {
+        public EfCoreSocialMediaRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
