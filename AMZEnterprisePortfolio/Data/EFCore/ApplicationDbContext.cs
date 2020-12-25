e713899cb81bc2ef78eae3077c27f4b38b0431d2
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2020-12-25 13:49:24 +0330

    Add ApplicationDbContext.cs and ef core Migrations

---
+++ AMZEnterprisePortfolio/Data/EFCore/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using AMZEnterprisePortfolio.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AMZEnterprisePortfolio.Data.EFCore
+{
+    /// <summary>
+    /// Application database context
+    /// </summary>
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Favor> Favors { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<Resume> Resumes { get; set; }
+        public DbSet<Setting> Settings { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+    }
+}
