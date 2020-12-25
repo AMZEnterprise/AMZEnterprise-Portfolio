4d7d68cfe841a9154cdc45aaa47e7007fd4497ea
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2020-12-25 13:53:31 +0330

    Add EfCoreSettingRepository.cs

---
+++ AMZEnterprisePortfolio/Data/EFCore/EfCoreSettingRepository.cs
@@ -0,0 +1,14 @@
+﻿using AMZEnterprisePortfolio.Models;
+
+namespace AMZEnterprisePortfolio.Data.EFCore
+{
+    /// <summary>
+    /// Setting repository ef core implementation
+    /// </summary>
+    public class EfCoreSettingRepository : EfCoreRepository<Setting, ApplicationDbContext>
+    {
+        public EfCoreSettingRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
