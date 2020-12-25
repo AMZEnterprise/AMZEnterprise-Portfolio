63ac4396022baa86926d75cd909f981620a0fc80
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2020-12-25 13:44:14 +0330

    Add Skill.cs model

---
+++ AMZEnterprisePortfolio/Models/Skill.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AMZEnterprisePortfolio.Data;
+
+namespace AMZEnterprisePortfolio.Models
+{
+    /// <summary>
+    /// Skill entity
+    /// </summary>
+    public class Skill : IEntity
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string Title { get; set; }
+        [Range(0,100)]
+        public int Percent { get; set; }
+        [Display(Name = "Skill Type")]
+        public SkillType SkillType { get; set; }
+    }
+}
---
+++ AMZEnterprisePortfolio/Models/SkillType.cs
@@ -0,0 +1,11 @@
+﻿namespace AMZEnterprisePortfolio.Models
+{
+    /// <summary>
+    /// Skill Type
+    /// </summary>
+    public enum SkillType
+    {
+        ProcessBar = 1000,
+        Stars = 2000
+    }
+}
