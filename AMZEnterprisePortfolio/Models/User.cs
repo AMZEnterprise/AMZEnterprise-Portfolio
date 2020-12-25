12fc73a3bfce53884115b58e5e4c839f1d84072a
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2020-12-25 13:43:13 +0330

    Add User.cs model

---
+++ AMZEnterprisePortfolio/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AMZEnterprisePortfolio.Models
+{
+    /// <summary>
+    /// User entity
+    /// </summary>
+    public class User : IdentityUser
+    {
+    }
+}
