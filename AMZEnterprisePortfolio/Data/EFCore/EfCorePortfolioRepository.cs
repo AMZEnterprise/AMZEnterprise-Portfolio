4b26e0e9ca8b2eb2abfe72b37b514375bdeb93ed
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2020-12-25 13:53:18 +0330

    Add EfCorePortfolioRepository.cs

---
+++ AMZEnterprisePortfolio/Data/EFCore/EfCorePortfolioRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AMZEnterprisePortfolio.Models;
+
+namespace AMZEnterprisePortfolio.Data.EFCore
+{
+    /// <summary>
+    /// Portfolio repository ef core implementation
+    /// </summary>
+    public class EfCorePortfolioRepository : EfCoreRepository<Portfolio, ApplicationDbContext>
+    {
+        public EfCorePortfolioRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
