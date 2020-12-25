3b12cd7ad789cc23e558e430786ae328d56919cd
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2020-12-25 13:53:56 +0330

    Add EfCoreResumeRepository.cs

---
+++ AMZEnterprisePortfolio/Data/EFCore/EfCoreResumeRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AMZEnterprisePortfolio.Models;
+
+namespace AMZEnterprisePortfolio.Data.EFCore
+{
+    /// <summary>
+    /// Resume repository ef core implementation
+    /// </summary>
+    public class EfCoreResumeRepository : EfCoreRepository<Resume, ApplicationDbContext>
+    {
+        public EfCoreResumeRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
