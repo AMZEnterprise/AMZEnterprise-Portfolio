c8a201fe3a8ab0d8f6bd332067fac18f5a9c5f12
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2020-12-25 13:47:32 +0330

    Add IEntity.cs, used for base entity

---
+++ AMZEnterprisePortfolio/Data/IEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMZEnterprisePortfolio.Data
+{
+    /// <summary>
+    /// Base entity
+    /// </summary>
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
