29c36ea467386ca31759a409042d8cb1fcaceb86
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2020-12-25 13:54:21 +0330

    Add EfCoreFavorRepository.cs

---
+++ AMZEnterprisePortfolio/Data/EFCore/EfCoreFavorRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AMZEnterprisePortfolio.Models;
+
+namespace AMZEnterprisePortfolio.Data.EFCore
+{
+    /// <summary>
+    /// Favor repository ef core implementation
+    /// </summary>
+    public class EfCoreFavorRepository : EfCoreRepository<Favor, ApplicationDbContext>
+    {
+        public EfCoreFavorRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
