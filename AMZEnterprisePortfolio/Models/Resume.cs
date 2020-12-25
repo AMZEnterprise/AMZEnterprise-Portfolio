1f9a6296c0928823fb4758f358b82c63d8a101b4
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2020-12-25 13:46:03 +0330

    Add Resume.cs model

---
+++ AMZEnterprisePortfolio/Models/Resume.cs
@@ -0,0 +1,24 @@
+﻿using AMZEnterprisePortfolio.Data;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AMZEnterprisePortfolio.Models
+{
+    /// <summary>
+    /// Resume entity
+    /// </summary>
+    public class Resume : IEntity
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string Title { get; set; }
+        [Required]
+        [MaxLength(1000)]
+        public string Description { get; set; }
+        [Display(Name = "Resume Type")]
+        public ResumeType ResumeType { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+    }
+}
---
+++ AMZEnterprisePortfolio/Models/ResumeType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMZEnterprisePortfolio.Models
+{
+    /// <summary>
+    /// Resume type
+    /// </summary>
+    public enum ResumeType
+    {
+        Education = 1000,
+        Experience = 2000
+    }
+}
