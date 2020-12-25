3c395c7940daafdceb352f9822e85cf9c5a3d86e
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2020-12-25 13:52:42 +0330

    Add EfCoreRepository.cs, ef core generic repository implementation

---
+++ AMZEnterprisePortfolio/Data/EFCore/EfCoreRepository.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMZEnterprisePortfolio.Data.EFCore
+{
+    ///<inheritdoc/>
+    public class EfCoreRepository<TEntity, TContext> : IRepository<TEntity>
+    where TEntity : class, IEntity
+    where TContext : DbContext
+    {
+        private readonly TContext _context;
+
+        public EfCoreRepository(TContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<TEntity>> GetAll()
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+
+        /// <summary>
+        /// Get all data
+        /// </summary>
+        /// <returns>return all entity type data as queryable</returns>
+        public IQueryable<TEntity> GetAllAsQueryable()
+        {
+            return _context.Set<TEntity>().AsQueryable();
+        }
+
+        public async Task<TEntity> Get(int id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task<TEntity> Add(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<TEntity> Update(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<TEntity> Delete(int id)
+        {
+            var entity = await _context.Set<TEntity>().FindAsync(id);
+            if (entity == null)
+            {
+                return entity;
+            }
+
+            _context.Set<TEntity>().Remove(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        /// <summary>
+        /// Count all Entity type objects
+        /// </summary>
+        /// <returns>return number of all Entity type object</returns>
+        public async Task<int> Count()
+        {
+            return await _context.Set<TEntity>().CountAsync();
+        }
+    }
+}
