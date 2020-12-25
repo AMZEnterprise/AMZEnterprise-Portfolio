bd140423c43e91a6fe2864c57c0ddae24559196a
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2020-12-25 13:53:44 +0330

    Add EfCoreSkillRepository.cs

---
+++ AMZEnterprisePortfolio/Data/EFCore/EfCoreSkillRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AMZEnterprisePortfolio.Models;
+
+namespace AMZEnterprisePortfolio.Data.EFCore
+{
+    /// <summary>
+    /// Skill repository ef core implementation
+    /// </summary>
+    public class EfCoreSkillRepository : EfCoreRepository<Skill, ApplicationDbContext>
+    {
+        public EfCoreSkillRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
