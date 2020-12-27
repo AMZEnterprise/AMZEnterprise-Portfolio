e101fa7c48060720ca9695d88585f0f52d30e869
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2020-12-27 23:52:33 +0330

    Add LinqExtensions.cs

---
+++ AMZEnterprisePortfolio/Areas/Panel/Extensions/LinqExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace AMZEnterprisePortfolio.Areas.Panel.Extensions
+{
+    /// <summary>
+    /// Linq Extensions
+    /// </summary>
+    public static class LinqExtensions
+    {
+        /// <summary>
+        /// Linq query order direction
+        /// </summary>
+        public enum Order
+        {
+            Asc,
+            Desc
+        }
+
+        /// <summary>
+        /// Dynamically order linq query
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="query"></param>
+        /// <param name="orderByMember"></param>
+        /// <param name="direction"></param>
+        /// <returns></returns>
+
+        public static IQueryable<T> OrderByDynamic<T>(
+            this IQueryable<T> query,
+            string orderByMember,
+            Order direction)
+        {
+            var queryElementTypeParam = Expression.Parameter(typeof(T));
+
+            var memberAccess = Expression.PropertyOrField(queryElementTypeParam, orderByMember);
+
+            var keySelector = Expression.Lambda(memberAccess, queryElementTypeParam);
+
+            var orderBy = Expression.Call(
+                typeof(Queryable),
+                direction == Order.Asc ? "OrderBy" : "OrderByDescending",
+                new Type[] { typeof(T), memberAccess.Type },
+                query.Expression,
+                Expression.Quote(keySelector));
+
+            return query.Provider.CreateQuery<T>(orderBy);
+        }
+    }
+}
