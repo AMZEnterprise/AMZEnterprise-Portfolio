fb090adedaec9089abc63dc371dc57d6f9c85795
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2020-12-27 23:57:35 +0330

    Add ContactsController.cs and Index view

---
+++ AMZEnterprisePortfolio/Areas/Panel/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AMZEnterprisePortfolio.Areas.Panel.Controllers
+{
+    [Area("Panel")]
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
