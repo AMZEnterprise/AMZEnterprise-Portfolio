eca667f05107b5926e93653922315786bfb15eb1
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2020-12-27 18:22:50 +0330

    Add HomeVm.cs to ViewModels

---
+++ AMZEnterprisePortfolio/Models/ViewModels/HomeVm.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace AMZEnterprisePortfolio.Models.ViewModels
+{
+    /// <summary>
+    /// Home ViewModel
+    /// </summary>
+    public class HomeVm
+    {
+        public Contact Contact { get; set; }
+        public Setting Setting { get; set; }
+        public IList<SocialMedia>SocialMedias { get; set; }
+        public IList<Favor> Favors { get; set; }
+        public IList<Resume> Resumes { get; set; }
+        public IList<Skill> Skills { get; set; }
+        public IList<Portfolio> Portfolios { get; set; }
+    }
+}
