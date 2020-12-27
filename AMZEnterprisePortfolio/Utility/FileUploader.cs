ccc3b7bce14bb3abc6d3ca273ec7e0c5966cafe8
Ali Momenzadeh <52137131+AMZEnterprise@users.noreply.github.com>
2020-12-27 18:23:40 +0330

    Add file uploader utility

---
+++ AMZEnterprisePortfolio/Utility/FileUploader.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace AMZEnterprisePortfolio.Utility
+{
+    ///<inheritdoc/>
+    public class FileUploader : IFileUploader
+    {
+        private async Task Upload(IFormFile file, string uploadPath)
+        {
+            Directory.CreateDirectory(uploadPath);
+
+            var fullPath = Path.Combine(uploadPath, file.FileName);
+
+            try
+            {
+                using (var fs = new FileStream(fullPath, FileMode.Create))
+                {
+                    await file.CopyToAsync(fs);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public async Task UploadMedia(IFormFileCollection files, string webRootPath, string filePath)
+        {
+            string uploadPath = webRootPath + "\\" + "uploads" + "\\" + filePath;
+            foreach (var file in files)
+            {
+                await Upload(file, uploadPath);
+            }
+        }
+
+        public void DeleteMedia(string webRootPath, string filePath)
+        {
+
+            string uploadPath = webRootPath + "\\" + "uploads" + "\\" + filePath;
+            try
+            {
+                if (Directory.Exists(uploadPath))
+                {
+                    Directory.Delete(uploadPath, true);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public string GetFileSource(string webRootPath, string filePath)
+        {
+            string uploadPath = webRootPath + "\\" + "uploads" + "\\" + filePath;
+
+            try
+            {
+                var files = Directory.GetFiles(
+                    uploadPath,
+                    "*.*",
+                    SearchOption.TopDirectoryOnly);
+
+                if (files.Length > 0 && files[0] != null)
+                {
+                    var path = "\\" + "uploads" + "\\" + filePath + "\\" + Path.GetFileName(files[0]);
+
+                    return path.Replace(@"\", "/");
+                }
+            }
+            catch { }
+
+            return null;
+        }
+    }
+
+}
---
+++ AMZEnterprisePortfolio/Utility/IFileUploader.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace AMZEnterprisePortfolio.Utility
+{
+    /// <summary>
+    /// File uploader
+    /// </summary>
+    public interface IFileUploader
+    {
+        /// <summary>
+        /// Upload media file
+        /// </summary>
+        /// <param name="files">files collection</param>
+        /// <param name="webRootPath">website default root path</param>
+        /// <param name="filePath">file uniq (GUID) path</param>
+        /// <returns></returns>
+        Task UploadMedia(IFormFileCollection files, string webRootPath, string filePath);
+        /// <summary>
+        /// Delete media file
+        /// </summary>
+        /// <param name="webRootPath">website default root path</param>
+        /// <param name="filePath">file uniq (GUID) path</param>
+        void DeleteMedia(string webRootPath, string filePath);
+        /// <summary>
+        /// Returns media file path for view
+        /// </summary>
+        /// <param name="webRootPath">website default root path</param>
+        /// <param name="filePath">file uniq (GUID) path</param>
+        /// <returns></returns>
+        string GetFileSource(string webRootPath, string filePath);
+    }
+}
